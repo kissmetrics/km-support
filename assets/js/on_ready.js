6ff23e7632da5550af8765cbc74eb0f6a1bfff5a
Nemo Chu <nemochu@Nemos-MacBook-Pro.local>
2013-08-05 15:46:30 -0700

    Grabbing latest changes and syncing timestamps

--- assets/js/on_ready.js
+++ assets/js/on_ready.js
@@ -14,4 +14,17 @@ $(document).ready(function() {
   // Server-side tracking
   var tracking_params = {'Support Article Viewed URL':'{{page.url}}','Support Article Viewed Title':'{{page.title}}'};
   km_track('Viewed Support Article', $.extend({}, tracking_params, utmParams));
+
+  // Optimizely
+  // https://www.optimizely.com/docs/api
+  var exps = window['optimizely'].data.state.activeExperiments;
+  var optimizely_params = {};
+  for (var i=0;i<exps.length;i++) {
+    var exp = exps[i];
+    var exp_name = "Optimizely - " + window['optimizely'].data.experiments[exp].name
+    var var_name = window['optimizely'].data.state.variationNamesMap[exp]
+
+    optimizely_params[exp_name] = var_name
+  }
+  km_track('Viewed Optimizely Experiment', optimizely_params)
 });
