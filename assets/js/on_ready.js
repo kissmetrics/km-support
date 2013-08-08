d9c105ec6e669fd3fe1201046ccb738f498fa81c
Eric Fung <eskfung@gmail.com>
2013-08-08 11:10:53 -0700

    Changing parameter name; hopefully this goes through.

--- assets/js/on_ready.js
+++ assets/js/on_ready.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     var optimizely_params = {};
     for (var i=0;i<exps.length;i++) {
       var exp = exps[i];
-      var exp_name = "Optimizely - " + window['optimizely'].data.experiments[exp].name
+      var exp_name = "Optimizely " + window['optimizely'].data.experiments[exp].name
       var var_name = window['optimizely'].data.state.variationNamesMap[exp]
 
       optimizely_params[exp_name] = var_name
