26aed14271dfaed6158cc69a010eeef4133bc7b4
Eric Fung <eskfung@gmail.com>
2014-01-15 15:24:53 -0800

    Adding feedback form via JS

--- assets/js/need_help.js
+++ assets/js/need_help.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  $(".rightcrumb").append();
-}
+  var item = $('<form id="feedback_form"><h3>How can we make this page better for you?</h3><textarea id="feedback_text" name="feedback_text">Your suggestions, ideas, complaints will greatly help us.</textarea><input id="ni_cookie" name="identity" style="visibility:hidden"></input><p style="display: block; visibility: visible; zoom: 1; opacity: 1;"><a href="#" class="small primary button blue">Send Feedback</a></p></form>')
+  $("#feedback").append(item);
+});
