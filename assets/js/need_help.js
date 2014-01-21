275b5f14a3894d5657ecf1b5ab60e3bdcf49f99d
Eric Fung <eskfung@gmail.com>
2014-01-20 17:11:13 -0800

    Wire Need Help button behavior to the modal.

--- assets/js/need_help.js
+++ assets/js/need_help.js
@@ -1,31 +1,28 @@
 $(document).ready(function() {
-  var item = $('<form id="feedback_form"><h3>How can we make this page better for you?</h3><textarea id="feedback_text" name="feedback_text">Your suggestions, ideas, complaints will greatly help us.</textarea><input id="email" name="email"></input><p style="display: block; visibility: visible; zoom: 1; opacity: 1;"><a id="submit_feedback" href="#" class="small primary button blue">Send Feedback</a></p></form>')
-  $("#feedback").append(item);
-
-  var getID = function() {
-    try{KM.gc('ni');}catch(e){return "unidentified";}
-  }
-
-  $("#submit_feedback").click(function(e) {
+  $('#feedbackModal button[type=submit]').click(function(e) {
     e.preventDefault();
-    $("#feedback_form").submit();
+    $("#feedbackForm").submit();
   });
 
-  $("#feedback_form").submit(function(e) {
+  $("#feedbackForm").submit(function(e) {
     e.preventDefault();
+    // Validate form fields?
 
     $.ajax({
       type: "post",
       url: "https://middleman.kissmetrics.com/helpscout.create_conversation",
       data: {
         url: document.location.href,
-        message: $("#feedback_text").val(),
-        identity: getID()
+        message: $("#feedbackText").val(),
+        email: $("#feedbackEmail").val(),
+        identity: (function(){try{KM.gc('ni');}catch(e){return "unidentified";}})()
       },
       success: function() {
         alert("BOOSH");
       },
       dataType: "json"
     });
+
+    // Replace modal body with success message
   });
 });
